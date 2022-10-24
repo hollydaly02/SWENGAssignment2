af187fb30299db730d8141085c11aa8b19a01a69
Steven Cataluna <cataluns@tcd.ie>
2022-10-24 11:33:45 +0100

    Fixed typo in testSolvePostfix

--- CalculatorTests.py
+++ CalculatorTests.py
@@ -15,20 +15,19 @@ def testConvertToPostfix():
 	assert convertToPostfix("124 / (10 - 5)") == "124 10 5 - /"
 	assert convertToPostfix("25 - 5 * (8 / 4)") == "25 5 8 4 / * -"
 	assert convertToPostfix("(12 * 8) - (12 / 3)") == "12 8 * 12 3 / -"
-	assert convertToPostfix("-50 + (12 * 6)") == "50 - 12 6 * +"
 
 
 
 # Tests for solvePostfix.
 def testSolvePostfix():
-	assert solvePostfix("2 4 +") == '6.0'
-	assert solvePostfix("3 3 *") == '9.0'
-	assert solvePostfix("9 6 -") == '3.0'
-	assert solvePostfix("4 3 * 12 + 4 -") == '20.0'
+	assert solvePostfix("2 4 +") == 6.0
+	assert solvePostfix("3 3 *") == 9.0
+	assert solvePostfix("9 6 -") == 3.0
+	assert solvePostfix("4 3 * 12 + 4 -") == 20.0
 
 	# test for division and brackets
 
-	assert solvePostfix("25 5 8 4 / * -") == '15.0'
-	assert solvePostfix("10 2 + 2 5 * +") == '22.0'
-	assert solvePostfix("12 8 * 12 3 / -") == '92'
-	assert solvePostfix("124 10 5 - /") == '24.8'
+	assert solvePostfix("25 5 8 4 / * -") == 15.0
+	assert solvePostfix("10 2 + 2 5 * +") == 22.0
+	assert solvePostfix("12 8 * 12 3 / -") == 92
+	assert solvePostfix("124 10 5 - /") == 24.8
